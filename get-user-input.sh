0b6046fa7a21029cb0502d50251de5674265bb0e
joey1089 <josephvpeter7@gmail.com>
2024-04-02 23:13:58 -0400

    fn added and updated

--- get-user-input.sh
+++ get-user-input.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 # This gets the user input & prints it out with the variables
 # before running this script give run permissions
+set -x  # used only for debugging the script
+# set -e # used to exit immediately when a cmd fails and it exits with error
+echo "Today is " `date`
+echo "Your shell is " `ps`
+echo $PWD
+get_ur_info() {
+    echo "what is your name ? "
+    read name
+    echo $name
+    echo "where do you live ? "
+    read residence
+    echo $residence
+    echo "We got your name $name and you live in $residence nice!"
+}
+get_ur_working_directory() {
+    echo $PWD
+}
+echo "Here we go $(get_ur_info)"
+echo "Your current working directory is $(get_ur_working_directory)"
 
-echo "what is your name ?: "
-read name
-echo "where do you live ?: "
-read residence
-echo "your name is :$name"
-echo "you live in :$residence"
+
+
+
+

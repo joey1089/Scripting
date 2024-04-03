bbc3389e2ba02e5364b7feb2d21dcbe8cbba90e5
joey1089 <josephvpeter7@gmail.com>
2024-04-02 21:27:10 -0400

    Input & output sample

---
+++ get-user-input.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This gets the user input
+
+echo "what is your name ?: "
+read name
+echo "where do you live ?: "
+read residence
+echo "your name is :$name"
+echo "you live in :$residence"
